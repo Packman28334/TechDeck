c8c325abb2ffc0929af44946993cf77ce1fe58f6
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2025-05-23 17:13:50 -0400

    added socket connection and blackout plans, also have all command we plan to add to the system listed with docs

--- backend/subsystems/mixer.py
+++ backend/subsystems/mixer.py
@@ -1,10 +1,8 @@
 
 import socket
-
 class MixerSubsystem:
     def __init__(self, ip_address: str):
         self.ip_address: str = ip_address
-
         self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.settimeout(5)
         self.socket.connect((self.ip_address, 49280))
@@ -15,19 +13,23 @@ class MixerSubsystem:
         }
 
     def enter_blackout(self):
-        pass
+        # Uses Mute Group 3 to Disable all Wanted Mics (The Blackout Mute Group)
+        command = ("set MIXER:Current/MuteMaster/On 2 0 1 \n").encode() # Mute Group 3 ON
+        socket.sendall(command)
 
     def exit_blackout(self):
-        pass
+        # Undoes enter_blackout
+        command = ("set MIXER:Current/MuteMaster/On 2 0 0 \n").encode() # Mute Group 3 OFF
+        socket.sendall(command)
 
     def run_command(self, command: dict):
         match command["id"]:
             case "enable_channels":
                 for channel in command["channels"]: # ex. command["channels"] = [0, 5, 8]
-                    pass # TODO
+                    pass #Todo
             case "disable_channels":
                 for channel in command["channels"]: # ex. command["channels"] = [0, 5, 8]
-                    pass # TODO
+                    pass #Todo
             case "set_faders_on_channels":
                 for channel, value in command["channels"].items(): # ex. command["channels"] = {0: -32768, 5: -5000, 8: 0}
-                    pass # TODO
+                    pass #Todo
