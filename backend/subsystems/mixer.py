d8fce50b9a1a79ccd127c2184fc9eb12816da021
Zachary Schultz <Zach.Schultz.2.0.1@gmail.com>
2025-05-24 17:55:49 -0400

    Added Mix and Mute Groups for enable_channels

--- backend/subsystems/mixer.py
+++ backend/subsystems/mixer.py
@@ -31,13 +31,17 @@ class MixerSubsystem:
     def identify_channel(self, channel: str) -> tuple[str, int]: # type, number
         channel = str(channel) # just in case
         if channel.startswith("ST"): # ST = Stereo
-            return "StInCh", int(channel.removeprefix("ST"))
+            return "StInCh/Fader", int(channel.removeprefix("ST"))
         elif channel.startswith("DCA"): # DCA = Groups
-            return "DCA", int(channel.removeprefix("DCA"))
+            return "DCA/Fader", int(channel.removeprefix("DCA"))
+        elif channel.startswith("MIX"): # MTRX = Matrix
+            return "Mix/Fader", int(channel.removeprefix("MIX"))
         elif channel.startswith("MTRX"): # MTRX = Matrix
-            return "Mtrx", int(channel.removeprefix("MTRX"))
+            return "Mtrx/Fader", int(channel.removeprefix("MTRX"))
+        elif channel.startswith("MUTE"): # MUTE = Mute Groups
+            return "MuteMaster", int(channel.removeprefix("MUTE"))
         else:
-            return "InCh", int(channel) # InCh = Input Channel
+            return "InCh/Fader", int(channel) # InCh = Input Channel
 
     def run_command(self, command: dict):
         match command["action"]:
@@ -45,30 +49,36 @@ class MixerSubsystem:
                 commands = []
                 for channel in command["channels"]:
                     channel_type, channel_number = self.identify_channel(channel)
-                    commands.append(f"set MIXER:Current/{channel_type}/Fader/On {channel_number} 0 1")
+                    commands.append(f"set MIXER:Current/{channel_type}/On {channel_number} 0 1")
                 self.send_requests(commands)
 
-            case "disable_channels":# Turns off any Input/Output Channel 
+            case "disable_channels": # Turns off any Input/Output Channel 
                 commands = []
                 for channel in command["channels"]:
                     channel_type, channel_number = self.identify_channel(channel)
-                    commands.append(f"set MIXER:Current/{channel_type}/Fader/On {channel_number} 0 0")
+                    commands.append(f"set MIXER:Current/{channel_type}/On {channel_number} 0 0")
                 self.send_requests(commands)
-
             case "set_faders_on_channels":
                 commands = []
                 for channel, value in command["channels"].items(): # ex. command["channels"] = {0: -inf, 5: -5, 8: 0, 17: -138} - provide a value in decibels from -138 to 10, or -inf
                     channel_type, channel_number = self.identify_channel(channel)
-                    commands.append(f"set MIXER:Current/{channel_type}/Fader/Level {channel_number} 0 {-32768 if value == '-inf' else value*100}")
+                    commands.append(f"set MIXER:Current/{channel_type}/Level {channel_number} 0 {-32768 if value == '-inf' else value*100}")
                 self.send_requests(commands)
+                
+            case "change_scene": # Change used scene
+                # do we want scene value to been in channel or do we want to store it somewhre else?
+                pass #Todo
+            
 
 
 # For Testing
 if __name__ == "__main__":
     TestCommands: list[dict] = [
-        {"subsystem": "mixer", "action": "enable_channels", "channels": {"DCA0", "ST1", "8"}},
-        {"subsystem": "mixer", "action": "disable_channels", "channels": {"DCA0", "ST1", "8"}},
-        {"subsystem": "mixer", "action": "set_faders_on_channels", "channels": {"DCA0":10, "ST1":-5, "8":"-inf"}}
+        {"subsystem": "mixer", "action": "enable_channels", "channels": {"DCA0", "ST1", "8", "MIX0"}},
+        {"subsystem": "mixer", "action": "disable_channels", "channels": {"DCA0", "ST1", "8", "MIX0"}},
+        {"subsystem": "mixer", "action": "set_faders_on_channels", "channels": {"DCA0":10, "ST1":-5, "8":"-inf"}},
+        {"subsystem": "mixer", "action": "enable_channels", "channels": {"MUTE0"}},
+        {"subsystem": "mixer", "action": "disable_channels", "channels": {"MUTE0"}}
     ]
 
     Mixer = MixerSubsystem("127.0.0.1")
