e7ee1277df3c629c92e24965b87d45494149396a
Packman28334 <packmanthecoder@gmail.com>
2025-05-27 11:35:11 -0400

    Improve homepage

--- frontend/techdeck/lib/homepage.dart
+++ frontend/techdeck/lib/homepage.dart
@@ -7,16 +7,18 @@ class TechDeckHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 18, 18, 18),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Tech Deck", style: TextStyle(fontSize: 96, fontWeight: FontWeight.w900)),
+            SizedBox(height: 80),
             Row(mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                MoonButton(label: Text("New Show"), onTap: () {}, backgroundColor: MoonColors.dark.beerus),
+                MoonButton(label: Text("New Show"), onTap: () {}, backgroundColor: Color.fromARGB(40, 255, 255, 255), buttonSize: MoonButtonSize.xl),
                 SizedBox(width: 30),
-                MoonButton(label: Text("Load Show"), onTap: () {}, backgroundColor: MoonColors.dark.piccolo)
+                MoonButton(label: const Text("Load Show"), onTap: () {}, backgroundColor: MoonColors.dark.piccolo, buttonSize: MoonButtonSize.xl)
             ]),
           ],
         ),
