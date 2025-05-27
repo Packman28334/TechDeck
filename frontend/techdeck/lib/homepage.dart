117678654c1f7e626cf3fd21a4ede551f0023615
Packman28334 <packmanthecoder@gmail.com>
2025-05-27 16:25:19 -0400

    Experimenting with dialog boxes

--- frontend/techdeck/lib/homepage.dart
+++ frontend/techdeck/lib/homepage.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:moon_design/moon_design.dart';
 
+void openNewShowDialog(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  showDialog(context: context, barrierDismissible: false, builder: (BuildContext context) {return SimpleDialog(
+    title: const Text("New Show"),
+    children: [
+      SizedBox(width: screenWidth * 0.5),
+      MoonButton(label: const Text("Create"), onTap: () {createNewShow(context); Navigator.pop(context);}, width: screenWidth*0.2)
+    ],
+  );});
+}
+
+void createNewShow(BuildContext context) {
+
+}
+
 class TechDeckHomePage extends StatelessWidget {
   const TechDeckHomePage({super.key});
 
@@ -16,9 +31,9 @@ class TechDeckHomePage extends StatelessWidget {
             SizedBox(height: 80),
             Row(mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                MoonButton(label: Text("New Show"), onTap: () {}, backgroundColor: Color.fromARGB(40, 255, 255, 255), buttonSize: MoonButtonSize.xl),
+                MoonButton(label: Text("New Show"), onTap: () {openNewShowDialog(context);}, backgroundColor: Color.fromARGB(40, 255, 255, 255), buttonSize: MoonButtonSize.xl),
                 SizedBox(width: 30),
-                MoonButton(label: const Text("Load Show"), onTap: () {}, backgroundColor: MoonColors.dark.piccolo, buttonSize: MoonButtonSize.xl)
+                MoonButton(label: const Text("Load Show"), onTap: () {}, backgroundColor: MoonColors.dark.piccolo, buttonSize: MoonButtonSize.xl),
             ]),
           ],
         ),
