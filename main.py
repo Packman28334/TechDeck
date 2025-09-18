7685ac7a66a502b139d737f030171c716449ec56
Packman28334 <packmanthecoder@gmail.com>
2025-09-18 11:57:45 -0400

    css is weird but i fixed the problems

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def component(component: str):
                 const shadow = this.attachShadow({mode: "open"});
 
                 shadow.innerHTML = `
-                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
                     <link rel="stylesheet" href="/assets/global.css" />
                     <link rel="stylesheet" href="/component/$COMPONENT$.css" />
                     $CONTENT$
